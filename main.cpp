ddbddd654d6da3fcc8f50cef49bd62e5b399e85c
AquIce <timeo.perrinjaquet@gmail.com>
2024-03-20 09:03:42 +0100

    Added event loop

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ const int SCREEN_HEIGHT = 400;
 int main(int argc, char* argv[]) {
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
+	SDL_Event event;
 
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
@@ -18,7 +19,14 @@ int main(int argc, char* argv[]) {
 	SDL_RenderDrawPoint(renderer, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
 	SDL_RenderPresent(renderer);
-	SDL_Delay(10000);
+
+	while(true) {
+		if(SDL_PollEvent(&event)) {
+			if(event.type == SDL_QUIT) {
+				break;
+			}
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
